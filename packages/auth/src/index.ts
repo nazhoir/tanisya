c59af1d56d54ecf126ec27672266c4dd3e7753b6
Nazhoir <nazhoirulamin031201@gmail.com>
2026-02-27 06:34:20 +0700

    try fix

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -4,14 +4,11 @@ import { env } from "@tanisya/env/server";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { nextCookies } from "better-auth/next-js";
-import dotenv from "dotenv";
-dotenv.config({
-  path: "../../apps/web/.env",
-});
+
+
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
-
     schema: schema,
   }),
   trustedOrigins: [env.CORS_ORIGIN],
