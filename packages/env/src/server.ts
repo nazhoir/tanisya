0d63b0362328843c1d0d685c10ac8f6db1aae41b
Nazhoir <nazhoirulamin031201@gmail.com>
2026-02-27 06:19:45 +0700

    fixxxx

--- packages/env/src/server.ts
+++ packages/env/src/server.ts
@@ -1,14 +1,18 @@
-import "dotenv/config";
+import dotenv from "dotenv";
 import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
-
+dotenv.config({
+  path: "../../apps/web/.env",
+});
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().min(1),
     BETTER_AUTH_SECRET: z.string().min(32),
     BETTER_AUTH_URL: z.url(),
     CORS_ORIGIN: z.url(),
-    NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
+    NODE_ENV: z
+      .enum(["development", "production", "test"])
+      .default("development"),
   },
   runtimeEnv: process.env,
   emptyStringAsUndefined: true,
