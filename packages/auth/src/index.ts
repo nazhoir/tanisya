70bbd40243f55328b3e4c0478f53eb150d66bfeb
Nazhoir <nazhoirulamin031201@gmail.com>
2026-02-27 03:20:09 +0700

    first init

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,19 @@
+import { db } from "@tanisya/db";
+import * as schema from "@tanisya/db/schema/auth";
+import { env } from "@tanisya/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [nextCookies()],
+});
